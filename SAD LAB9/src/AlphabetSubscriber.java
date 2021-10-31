9038d0f90f94c11b4d29653f44b2f0a4a2a92977
pufask137 <63010510+pufask137@users.noreply.github.com>
2021-10-31 22:41:19 +0700

    Add files via upload

---
+++ SAD LAB9/src/AlphabetSubscriber.java
@@ -0,0 +1,34 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.concurrent.Flow;
+
+public class AlphabetSubscriber extends StringSubscriber {
+
+    private String workingDir;
+
+    AlphabetSubscriber() {
+        this.workingDir = "src\\resources\\Alphabet.txt";
+    }
+
+    @Override
+    public void onSubscribe(Flow.Subscription subscription) {}
+
+    @Override
+    public void onNext(Object item) {
+        try {
+            FileWriter fileWriter = new FileWriter(workingDir, true);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            printWriter.printf(item + "\n");
+            printWriter.close();
+            System.out.println("\t\tIt was received by Alphabet Subscriber.");
+        } catch (IOException ex) {
+            this.onError(ex);
+        }
+    }
+
+    @Override
+    public boolean checkContains(String item) {
+        return item.matches(".*[a-zA-Z].*");
+    }
+}
